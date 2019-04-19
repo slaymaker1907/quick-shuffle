5171c7d8b60a33d1487d41e4da5330054dfa6b30
Dyllon Gagnier <dyllongagnier@gmail.com>
2019-04-19 17:00:58 -0600

    Got code compiling on Linux.

--- quick-shuffle-gpu/include/cuda_ptr_cas.hpp
+++ quick-shuffle-gpu/include/cuda_ptr_cas.hpp
@@ -6,22 +6,24 @@
 namespace cuda_permute
 {
 
-template <class T>
-T* cuda_ptr_cas(T **address, T *compare, T *value) {
+// template <class T>
+#define T void
+__device__ T* cuda_ptr_cas(T **address, T *compare, T *value) {
     // all but one condition should be eliminated.
     if (sizeof(T*) == sizeof(unsigned long long int)) {
-        unsigned long long int *cast_address = (unsigned long long int)address;
+        unsigned long long int *cast_address = (unsigned long long int*)address;
         unsigned long long int cast_compare = (unsigned long long int)compare;
-        unsigned long long int case_value = (unsigned long long int)value;
+        unsigned long long int cast_value = (unsigned long long int)value;
         atomicCAS(cast_address, cast_compare, cast_value);
     } else {
         // assume it is the size of an int.
         unsigned int *cast_address = (unsigned int)address;
         unsigned int cast_compare = (unsigned int)compare;
-        unsigned int case_value = (unsigned int)value;
+        unsigned int cast_value = (unsigned int)value;
         atomicCAS(cast_address, cast_compare, cast_value);
     }
 }
+#undef T
 
 }
 #endif
--- quick-shuffle-gpu/src/cuda_polyfill.cpp
+++
@@ -1,1 +0,0 @@
-#include "cuda_polyfill.hpp"
